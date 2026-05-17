7aaacd6b5219b3e727447a9ce800cc70a45121eb
Username148576 <93710280+Username148576@users.noreply.github.com>
2026-05-17 17:15:03 +0000

    test prettier

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import { buildApp } from './app';
 
 const app = buildApp({ logger: true });
 const port = Number(process.env.PORT || 3000);
-const host  = process.env.HOST || '0.0.0.0';
+const host = process.env.HOST || '0.0.0.0';
+const x: number = 'string';
 
 async function start() {
   try {
